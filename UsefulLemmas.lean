f0a437d64a10a5e600626eb1f2daa5dead30f467
Colin Jones <137637335+Colin166@users.noreply.github.com>
2023-12-19 14:35:19 -0500

    Update UsefulLemmas.lean
    Added more lemmas and a tactics section

--- UsefulLemmas.lean
+++ UsefulLemmas.lean
@@ -14,6 +14,42 @@ import Mathlib.Data.Complex.Basic
 import Mathlib.Analysis.SpecialFunctions.Pow.Real
 import Mathlib.FieldTheory.Perfect
 
+section UsefulTactics
+/-!
+# Useful Tactics
+General
+ - have : used to write a intermediate step in the proof
+ - calc : produces a calc block that allows user to manipulate expression step-by-step
+Rearrangers / solvers
+ - ring : used to simplify expressions and complete goals, useful in have statements where you have to rearrange paranthesis
+ - field_simp : useful to simplify expressions with division in them
+ - norm_num : useful for expressions with numbers
+ - simp [*] : a generic tactic that acts like ring
+Logical
+ - tauto : solves many logical / propositional goals
+ - intro : produces a witness for implications or 'for all' / ∀ statements
+ - use : produces a subject to prove 'exists' / ∃ statements
+ - constructor : takes apart 'and' / ∧ statements in goals and 'iff' / ↔ statements
+ - left / right : used when the goal has an 'or' / ∨ statement; switches goal to that direction
+ - by_contra : changes proof to a proof by contradiction by assuming that the goal is not true and transforming it into a hypothesis in which the user can use to prove False
+ - assumption : sees if the goal matches any hypotheses that the user has, if it does, then it clears the goal
+ - apply : more specific form of assumption that takes a hypothesis and applies it to the goal
+ - exact : does the same thing as apply but fails if it doesn't solve the goal
+-/
+example : 2 + 4 = 3 + 3 := by norm_num
+example : (P∨Q) → R ↔ (P → R) ∧ (Q → R) := by
+  constructor -- breaks apart the iff statement
+  intro h -- takes the implication and produces a hypothesis
+  constructor -- breaks apart the and statement
+  intro p
+  have p' : P ∨ Q := by left; assumption -- produces a necessary hypothesis to use in implication
+  exact h p'
+  intro q
+  have q' : P ∨ Q := by right; apply q
+  exact h q'
+  intro h
+  tauto -- makes it easy for logical goals
+end UsefulTactics
 
 section Eq_Ne_Symm
 /-!
@@ -48,6 +84,7 @@ end add_sub_theorems
 section mul_div_theorems
 /-!
 # Multiplication and Division Theorems
+sq : x^2 = x*x
 div_self : If x ≠ 0 then x / x = 1
 div_mul_cancel : If y ≠ 0 then x / y * y = x
 mul_div_cancel : If y ≠ 0 then x * y / y = x
@@ -60,26 +97,21 @@ mul_inv_cancel : If a ≠ 0 then a * a⁻¹ = 1
 mul_div_mul_right : If a ≠ 0 then b * a / (c * a) = b / c
 -/
 variable {x y z : ℝ}
-example (h : x ≠ 0) (hy : y ≥ 0) : 2*x = x*y^2 → y = Real.sqrt 2 := by
-  intro h1
-  rw [mul_comm x, ← div_left_inj' h, mul_div_assoc, mul_div_assoc] at h1
-  rw [show x/x = 1 by apply div_self h, mul_one, mul_one] at h1
-  have h1' := by apply Eq.symm h1
-  have h2 : 2 = ((2:ℝ) ^ ((1:ℝ)/(2:ℝ) * 2)) := by norm_num
-  rw [Real.sqrt_eq_rpow]; nth_rw 1 [h2, h1]; norm_num
-  apply Eq.symm
-  have step1 :=
-    calc
-      (y ^ 2) ^ (1 / 2) = y ^ (2 * (1/2)) := by rw [← Real.rpow_mul hy]
-      _ = y := by norm_num
-  have twoeqtwo : (2:ℝ) = (2:ℕ) := by rfl
-  nth_rw 1 [twoeqtwo]
-
-
-
+example (hx : x ≠ 0) (hy : y ≥ 0) : 3*x = x*y^(2:ℕ) → y = Real.sqrt 3 := by
+  rw [mul_comm x (y^(2:ℕ))]
+  intro h
+  have h' := by apply mul_right_cancel₀ hx h
+  refine ((fun {x y} hx hy => (Real.sqrt_eq_iff_mul_self_eq hx hy).mpr) ?hx hy ?_).symm -- found from apply? tactic which allows you to let the computer figure out what to do
+  norm_num; have ysq : y*y = y^2 := by rw [← sq]; norm_num
+  rw [ysq]; exact Eq.symm h'
+example (hy : y ≠ 0) (hz : z ≠ 0) : (y^2 * z) * x / y / z = x*y := by
+  rw [div_div, ← inv_mul_eq_div, mul_assoc, show y^2 = y*y by rw [← sq]; simp [*], mul_inv];
+  repeat rw [← mul_assoc]
+  have step1 : y⁻¹ * z⁻¹ * y * y * z * x = (y⁻¹ * y) * y * (z⁻¹ * z) * x := by ring
+  rw [step1, show y⁻¹ * y = 1 by rw [mul_comm, mul_inv_cancel hy], show z⁻¹ * z = 1 by rw [mul_comm, mul_inv_cancel hz]]
+  simp [*]; rw [mul_comm]
 example (h : x ≠ 0) : x ^ ((x * y / x) + 1) = x * x ^ y := by
   rw [mul_comm, mul_div_cancel _ h, Real.rpow_add_one h, mul_comm]
-
 end mul_div_theorems
 
 
